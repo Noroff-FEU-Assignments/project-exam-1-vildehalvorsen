a6f5911c3f261f938eef0eaa9e8cc41a2d08731f
vildehalvorsen <vilde_ha96@hotmail.com>
2021-11-23 13:01:49 +0100

    update

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ rightArrow.addEventListener("click", function() {
     } else if (window.screen.width < 600) {
         pixel = 230;
     } else {
-        pixel = 600;
+        pixel = 220;
     }
 
     latestPosts.scrollLeft += pixel;
@@ -62,7 +62,7 @@ leftArrow.addEventListener("click", function() {
     } else if (window.screen.width < 600) {
         pixel = 230;
     } else {
-        pixel = 600;
+        pixel = 220;
     }
 
     latestPosts.scrollLeft -= pixel;
